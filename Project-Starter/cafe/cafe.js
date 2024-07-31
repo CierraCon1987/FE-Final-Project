0e9a96870e5333801709132b2c82236b275b4572
preethar333 <harpreetkaur3116@conestogac.on.ca>
2024-07-31 14:41:17 -0400

    update2

---
+++ Project-Starter/cafe/cafe.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+    //for preload images to browser
+    const preloadImages= [
+        "images/exoresso_info.jpg",
+        "images/latte_info.jpg",
+        "images/cappuccino_info.jpg",
+        "images/coffee_info.jpg",
+        "images/biscotti_info.jpg",
+        "images/scone_info.jpg"
+    ];
+
+    $.each(preloadImages,function (index,src) {
+        $('<img/>').attr('src',src);
+    });
+    //to add rollover images effect
+    $("ul li img").hover(
+        function() {
+            const infoImageSrc = $(this).attr("id");
+            const originalSrc= $ (this).attr("src");
+            $(this).data("original-src",originalSrc);
+            $(this).attr("src", infoImageSrc);
+        },
+        function(){
+            const originalSrc=$(this).data("original-src");
+            $(this).attr("src" , originalSrc);
+        }
+        );
+});
